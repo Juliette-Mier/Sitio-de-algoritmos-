a405968e72e97ecaa033fc35d77552d24b69c8d7
JULIETTE MIER <91492457+Juliette-Mier@users.noreply.github.com>
2021-10-11 18:19:04 +0000

    Hice el script para calcular promedios

---
+++ promedios.js
@@ -0,0 +1,23 @@
+//Este script realiza el calculo de un promedio de 5 calificaciones 
+//Autor: Paulina Juliette Mier Ancona 
+//Fecha:11 de Octubre del 20281
+function calcularpromedio () { 
+  var c1;
+  var c2;
+  var c3;
+  var c4:
+  var c5;
+  var promedio;
+  //3.Hacer c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 =10
+  c1 = 10;
+  c2 = 7.5;
+  c3 = 9;
+  c4 = 10;
+  c5 = 10;
+  //4.Hacer c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 =10 /5 = 46.5 = 9.3
+  promedio c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 = 10 /5
+  //5. Imprimir promedio 
+  alerta  (Tu promedio es: '' + promedio)
+ } //6. Fin
+
+ calcularpromedio();
