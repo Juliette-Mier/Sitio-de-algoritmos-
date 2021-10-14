05c0373d822576fb5f1698081f549c2c53efb5c1
JULIETTE MIER <91492457+Juliette-Mier@users.noreply.github.com>
2021-10-14 17:22:51 +0000

    Porcentaje

--- promedios.js
+++ promedios.js
@@ -9,11 +9,11 @@ function calcularpromedio () {
   var c5;
   var promedio;
   //3.Hacer c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 =10
-  c1 = 10;
-  c2 = 7.5;
-  c3 = 9;
-  c4 = 10;
-  c5 = 10;
+  c1 = parseFloat(document.getElementById("calificqacion1").value);
+  c2 =  parseFloat(document.getElementById("calificqacion2").value);
+  c3 =  parseFloat(document.getElementById("calificqacion3").value);
+  c4 =  parseFloat(document.getElementById("calificqacion4").value);
+  c5 =  parseFloat(document.getElementById("calificqacion5").value);
   //4.Hacer c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 =10 /5 = 46.5 = 9.3
   promedio c1 = 10, c2 = 7.5, c3 = 9, c4 = 10, c5 = 10 /5
   //5. Imprimir promedio 
